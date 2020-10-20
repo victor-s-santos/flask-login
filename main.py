2fdd4502fdfed22a335587adb269ba76454ab881
Victor Santos Silva <victor.s.silva@unesp.br>
2020-10-20 09:42:30 -0300

    commenting lines

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+from flask import render_template
 from app import app
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
 app.run(debug=True)
