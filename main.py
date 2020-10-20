1712d06c9fd66b66e7bf4f3b0e741a87d8867c4a
Victor Santos Silva <victor.s.silva@unesp.br>
2020-10-20 10:43:02 -0300

    user can be registered

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from flask import render_template
-from app import app
+from flask import render_template, request, redirect, url_for
+from app import app, db
+from app.models import User
+
+db.create_all()
 
 @app.route('/')
 def home():
@@ -7,6 +10,15 @@ def home():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        pwd = request.form['password']
+        if name and email and pwd:    
+            user = User(name, email, pwd)
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('home'))
     return render_template('register.html')
 
 @app.route('/login', methods=['GET', 'POST'])
